ce39488b59db4c649fa8b6167092100beb9c1886
Implax <juniourclottey@gmail.com>
2021-12-11 12:01:33 +0000

    this

--- model/db_credentials.php
+++ model/db_credentials.php
@@ -1,14 +1,14 @@
 <?php
     // PHP MyAdmin
-    // define("servername","eu-cdbr-west-02.cleardb.net");
-    // define("username","b82ea131993b8d");
-    // define("password","64e4893a");
-    // define("dbname","heroku_8fa64a8b89d5cb9");
+    define("servername","eu-cdbr-west-02.cleardb.net");
+    define("username","b82ea131993b8d");
+    define("password","64e4893a");
+    define("dbname","heroku_8fa64a8b89d5cb9");
 
-    define("servername","localhost");
-    define("username","root");
-    define("password","admin");
-    define("dbname","techbay");
+    // define("servername","localhost");
+    // define("username","root");
+    // define("password","admin");
+    // define("dbname","techbay_database_12462023");
 
     //Remote Database
     // define("servername","sql4.freemysqlhosting.net");
